79c8626f2150bbbf15d6ed99aeb7e4b4a8b80226
hkavc <80752805+hkavc@users.noreply.github.com>
2021-12-10 01:12:38 +0300

    Add files via upload

---
+++ Try_Catch_Finally_Mantıksal_Hatalar/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+namespace Try_Catch_Finally_Mantıksal_Hatalar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("bir sayı giriniz:");
+                int sayi = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("girmiş olduğunuz sayi" + sayi);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("hata" + ex.Message.ToString());
+
+            }
+            //finally
+            //{
+            //   Console.WriteLine("işeleminiz tamamlandı");
+            //}
+
+            try 
+            {
+                //int a=int.Parse(null);
+               // int a=int.Parse("test");
+                int a=int.Parse("-20000000000");
+            }
+            catch (ArgumentException ex)
+            {
+Console.WriteLine("boş değer girdiniz");
+Console.WriteLine(ex);
+
+            }
+            catch( FormatException ex)
+            {
+                Console.WriteLine("veri tipine ygun değil");
+Console.WriteLine(ex);
+
+            }
+     catch( OverflowException ex)
+            {
+                Console.WriteLine("kücük veta buyuk değer girdiniz");
+Console.WriteLine(ex);
+
+            }
+
+
+        }
+
+
+
+    }
+}
