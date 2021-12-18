390d7a5c29d2dc34a7c82a5f18fb9453066ab892
hkavc <80752805+hkavc@users.noreply.github.com>
2021-12-18 21:26:21 +0300

    Add files via upload

---
+++ If-ElseIf-Ternary-If/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace If_ElseIf_Ternary_If
+{
+    class Program 
+    {
+        static void Main (string[] args)
+        {
+int time = DateTime.Now.Hour;
+if (time>=6 && time <11)
+Console.WriteLine("Günaydın");
+else if (time <= 18)
+Console.WriteLine("İyi Geceler");
+else 
+Console.WriteLine("İyi Geceler");
+string sonuc = time<=18 ? "iyi günler": "iyi Geceler";
+sonuc = time >=6 &&  time<11 ? "günyadın" : time <=18? "iyi günler" : "iyi günler";
+Console.WriteLine(sonuc)
+
+        }
+    }
+}
