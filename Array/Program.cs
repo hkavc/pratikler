b15830fa557d96329598a59bf5ef1536384baf79
hkavc <80752805+hkavc@users.noreply.github.com>
2021-12-18 23:35:49 +0300

    Add files via upload

---
+++ Array/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //sort
+            int []sayiDizisi ={23,12,4,86,72,3,11,17};
+            Console.WriteLine("sırasız dizi");
+            foreach(var sayi in sayiDizisi)
+            Console.WriteLine(sayi);
+
+            Console.WriteLine("sıralı dizi");
+            Array.Sort(sayiDizisi);
+            foreach(var sayi in sayiDizisi)
+            Console.WriteLine(sayi);
+            //cleAr
+            Console.WriteLine("Array clear");
+            Array.Clear(sayiDizisi,2,2);
+            foreach(var sayi in sayiDizisi)
+            Console.WriteLine(sayi);
+
+            //reverse
+            Console.WriteLine("Array reverse");
+            Array.Reverse(sayiDizisi);
+             foreach(var sayi in sayiDizisi)
+            Console.WriteLine(sayi);
+
+              //ındexof
+            Console.WriteLine("Array ındexof");
+            Console.WriteLine(Array.IndexOf (sayiDizisi,23));
+              //resize
+            Console.WriteLine("Array resize");
+            Array.Resize<int>(ref sayiDizisi,9);
+            sayiDizisi[8]=99;
+
+
+             foreach(var sayi in sayiDizisi)
+            Console.WriteLine(sayi);
+
+
+
+
+
+
+
+        }
+    }
+}
