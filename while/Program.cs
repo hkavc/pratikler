71396bd42c283c180d09394424a7f1b5c145e159
hkavc <80752805+hkavc@users.noreply.github.com>
2021-12-20 16:47:03 +0300

    Add files via upload

---
+++ while/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace whilee
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            Console.Write("lütfen bir sayı giriniz");
+            int sayi= int.Parse(Console.ReadLine());
+            int sayac=1;
+            int toplam =0;
+            while (sayac<=sayi)
+            {
+                toplam+=sayac;
+                sayac++;
+
+            }
+              Console.Write(toplam/sayi);
+
+              char character='a';
+              while (character<'z')
+              {
+                     Console.Write(character);
+                     character++;
+              }
+
+        }
+    }
+}
